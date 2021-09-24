57a88f65467b2eb072dede184aebf2df5e7c6e5c
hyungmin19 <spartasparta819@gmail.com>
2021-09-24 10:36:21 +0900

    수업과제 및 코테 공부

--- 파이썬프로그래밍-수업/2주차-별문제.py
+++ 파이썬프로그래밍-수업/2주차-별문제.py
@@ -1,7 +1,11 @@
 import turtle as t
 
+t.setup(width=600, height = 600)
 t = t.Turtle()
 t.shape("turtle")
+
+input("Press any key to exit ...")
+
 
 # size = int(input("집 크기는 얼마로 할까요?"))
 
@@ -14,7 +18,7 @@ t.right(90)
 t.forward(100)
 t.right(90)
 
-turtle.done
+t.done
 
 # t.forward(size)
 # t.left(120)
